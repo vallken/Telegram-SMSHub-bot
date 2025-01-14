fa51a619b3e844d827336ded3250defa0d5380df
vallken <salvakennedy@gmail.com>
2025-01-14 12:18:30 +0700

    1.4

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { config } from "dotenv";
 import mongoose from "mongoose";
 import { Telegraf } from "telegraf";
 import botHandlers from "./bot.js";
+import express from "express";
 
 config();
 const bot = new Telegraf(process.env.TOKEN);
@@ -12,6 +13,19 @@ mongoose
   .catch((err) => console.error("MongoDB connection error:", err));
 
 botHandlers(bot);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 bot.launch();
 process.once("SIGINT", () => bot.stop("SIGINT"));
